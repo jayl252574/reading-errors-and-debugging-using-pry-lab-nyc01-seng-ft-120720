b2905e21ec0f148d8308d01ea47bcfd562cbfbb1
Jay Liu <jayl252574@gmail.com>
2020-11-23 04:57:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,6 @@
-# don't forget to add: require 'pry'
-
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
-end
-
-
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
-end
-
-def engage
-  puts state_log(date)
-  date = generate_star_date
+# dondescribe "the_final_frontier" do
+  it "`engage` should return the captain's log and crew greetings" do
+    expect(engage).to eq(["Hello Geordi.", "Hello Data.", "Hello Worf.", "Hello William.", "Hello Beverly.", "Hello Deanna."])
+    expect{ engage }.to output(/Captain's Log, star date /).to_stdout
+  end
 end
